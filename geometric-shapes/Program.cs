81fa57b7ada16aa00a09bda3a9e9aa84d9c42f2d
Tim Doren <dorentim@gmail.com>
2023-02-15 11:54:27 -0500

    Add project files.

---
+++ geometric-shapes/Program.cs
@@ -0,0 +1,33 @@
+﻿using geometric_shapes;
+
+Square s1 = new Square(5);
+Console.WriteLine($"The Perimeter is {s1.Perimeter()}");
+Console.WriteLine($"The Area is {s1.Area()}");
+Rect r1 = new Rect(3, 5);
+Console.WriteLine($"Perimeter is {r1.Perimeter()}");
+Console.WriteLine($"Area is {r1.Area()}");
+Quad q1 = new Quad(3, 4, 5, 6);
+Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine($"2 plus 3 equals {MathLib.Add(2,3)}");
+//Console.WriteLine($"10 minus 5 equals {MathLib.Subtract(10, 5)}");
+//Console.WriteLine($"10 times 5 equals {MathLib.Multiply(10, 5)}");
+//Console.WriteLine($"10 divided by 5 equals {MathLib.Division(10, 5)}");
+
+
+
+
+
+//var fullname = Person.Fullname("Tim","Doren");
+
+//Console.WriteLine(fullname);
---
+++ geometric-shapes/Rect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_shapes
+{
+    internal class Rect
+    {
+        public int LongSide { get; set; }
+        public int ShortSide { get; set; }
+
+        public int Perimeter()
+        {
+            return (2 * LongSide) + (2 * ShortSide);
+        }
+        public int Area()
+        {
+            return LongSide * ShortSide;
+        }
+        public Rect() { }
+        public Rect(int longSide, int shortSide)
+        {
+            LongSide = longSide;
+            ShortSide = shortSide;
+        }
+    }
+}
